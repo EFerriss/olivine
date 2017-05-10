bcbc6d275e2bf649c1a84795c8d09fe50b4865b7
Ferriss <elizabeth.ferriss@gmail.com>
2017-05-10 12:30:36 -0400

    updated SC1-2 dehydration profiles

--- olivine/SuppFig4_SC_dehydration_profiles_linear.py
+++ olivine/SuppFig4_SC_dehydration_profiles_linear.py
@@ -4,7 +4,8 @@ Created on Thu Mar 16 21:25:29 2017
 
 @author: Elizabeth
 
-Dehydration profiles for San Carlos olivine SC1-2
+Dehydration profiles for San Carlos olivine SC1-2 determined using 
+linear baselines
 
 Data details and peak heights are in SanCarlos_spectra.py. Baselines
 were created in SanCarlos_baselines.py and are stored in the same
@@ -13,35 +14,25 @@ folder as the original FTIR data.
 from __future__ import print_function, division
 from olivine.SanCarlos import SanCarlos_spectra as SC
 import matplotlib.pyplot as plt
-from pynams import dlib
+from olivine import high_ending
 import itertools
 import numpy as np
-from olivine import high_ending
-   
-fastdiffusion = dlib.KM98_fast.whatIsD(800, printout=False)[0:3]
+
+water = 14.
 peaks = SC.peaks
 wb2 = SC.wb_800C_hyd
 wb_list = [SC.wb_800C_hyd, SC.wb_800C_1hr, SC.wb_800C_3hr,
            SC.wb_800C_7hr, SC.wb_800C_13hr, SC.wb_800C_19hr, SC.wb_800C_43hr,
            SC.wb_800C_68hr]
 
-# set up areas and convert them to water concentrations
-conversion_factor_area2water = 0.6 # see Table1_concentrations.py
 for wb in wb_list:
     wb.get_baselines(baseline_ending=high_ending)
     wb.make_areas()
     wb.make_peakheights(peaks=peaks)
-    for prof in wb.profiles:
-        prof.areas = list(prof.areas * conversion_factor_area2water)
     wb.areas = [prof.areas for prof in wb.profiles]
     wb.peak_heights = [prof.peak_heights for prof in wb.profiles]
-# not sure why I need to do this twice. There must be a bug somewhere.
-wb = wb_list[0]
-for prof in wb.profiles:
-    prof.areas = list(prof.areas * conversion_factor_area2water)
 
-#%%
-#% the figure
+#%% the figure
 style2 = {'color':'grey', 'marker':'o', 'linestyle': 'none', 'markersize':8,
           'label': 'hydrated', 'alpha':0.5,}
 style1 = {'color':'r', 'marker':'+', 'linestyle':'none', 'markersize':9,
@@ -98,45 +89,45 @@ axes[-1][0].set_ylabel('bulk hydrogen\n(ppm H$_2$O)')
 axes[0][0].set_ylabel('[Si-Fe$^{2+}$] peak\nheight (cm$^{-1}$)')
 axes[1][0].set_ylabel('[Ti-3525] peak\nheight (cm$^{-1}$)')
 
-# axes limits
-ytops = [0.14, 0.25, 20]
+## axes limits
+ytops = [0.15, 0.25, 20]
 ybots = [0.0, 0, 0]
 for top, bot, ax3 in zip(ytops, ybots, axes):    
     for ax in ax3:
         ax.set_ylim(bot, top)
 
-# plot bulk H
-idx = 2
-for wb, style in zip(wb_list, styles):
-    wb.plot_areas_3panels(axes3=axes[idx], styles3=[style]*3, 
-                       centered=True, show_errorbars=False)
-
-for idx in range(2):
-    for wb, style in zip(wb_list, styles):
-        wb.plot_areas_3panels(axes3=axes[idx], styles3=[style]*3, 
-                               centered=True, show_errorbars=False, 
-                               peak_idx=idx, heights_instead=True)
-    axes[idx][1].set_title('')
-
-for ax, direction, raypath in zip(axes[-1], wb2.directions, wb2.raypaths):
-    ax.set_title(''.join(('|| ', direction, ', R || ', raypath)))
-
-
-# initial = mean of the initial across all profiles
 initials = [0]*3
-initials[2] = np.mean(list(itertools.chain(*wb2.areas)))
+initials[2] = 14.
 for idx in range(2):
     x, y = wb2.xy_picker(peak_idx=idx, wholeblock=False, heights_instead=True)
     initials[idx] = np.mean(list(itertools.chain(*y)))
 for ax3, init in zip(axes, initials):
     for ax in ax3:
         ax.plot(ax.get_xlim(), [init, init], ':', color=style2['color'])
+        
+wbdata = False
+scalewater = initials[2] / np.mean(list(itertools.chain(*wb2.areas)))
+idx = 2
+for wb, style in zip(wb_list, styles):
+    wb.plot_areas_3panels(axes3=axes[idx], styles3=[style]*3, 
+                       centered=True, show_errorbars=False, 
+                       wholeblock=wbdata, scale=scalewater)
 
-axes[2][2].legend(ncol=4, title='SC1-2 dehydration time', 
-   bbox_to_anchor=[0.7, 1], loc=1)
+for idx in range(2):
+    for wb, style in zip(wb_list, styles):
+        wb.plot_areas_3panels(axes3=axes[idx], styles3=[style]*3, 
+                               centered=True, show_errorbars=False, 
+                               peak_idx=idx, heights_instead=True,
+                               scale=1., wholeblock=wbdata)
+    axes[idx][1].set_title('')
+
+for ax, direction, raypath in zip(axes[-1], wb2.directions, wb2.raypaths):
+    ax.set_title(''.join(('|| ', direction, ', R || ', raypath)))
+
+
+axes[0][2].legend(ncol=4, title='SC1-2 dehydration time', 
+   bbox_to_anchor=(0.7, 0.35))
 
 fig.suptitle('Linear baselines')
 fig.savefig(SC.thisfolder+'\..\SuppFig4_SC_dehydration_profiles_linear.jpg', 
             dpi=400, format='jpg')        
-
-
